158c6456a3cc0e816dad874f11817af5b0267977
MFarooq22 <104709330+MFarooq22@users.noreply.github.com>
2023-11-08 12:29:05 +0500

    Update User.js

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,16 +1,20 @@
 // Schemas 
 const contactSchema = require('../models/contactSchema');
 
-const contactForm = (req, res) => {
-    const formData = req.body;
-    console.log(formData);
-
-    // Respond to the client
-    res.json({ message: req.body});
+const contactForm = async(req, res) => {
+    try {
+        const formData = req.body;
+        const contact = new contactSchema(formData);
+        await contact.save();
+        res.status(201).json({ message: 'Form submitted successfully' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
 };
 
 const test = (req, res) => {
-    res.send('hi')
+    res.send('hi');
 };
 
 module.exports = {test,contactForm};
