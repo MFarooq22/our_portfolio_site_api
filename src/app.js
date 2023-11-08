21d2fb632d65ccd1e1a31b654d4528bb377df288
MFarooq22 <104709330+MFarooq22@users.noreply.github.com>
2023-11-08 12:30:11 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,28 +9,12 @@ const UserRoutes = require("./routes/User");
 const app = express();
 
 // middlewares
-app.use('/api', UserRoutes);
 app.use(express.json());
+app.use('/api', UserRoutes);
 
 // Define the port for your server
 const PORT = process.env.PORT || 8000;
 
-// const contactSchema = require('./models/contactSchema');
-
-
-// app.post("/contact" , async(req, res) => {
-//     try {
-//     const formData = req.body
-//     const contact = new contactSchema(formData);
-//     await contact.save();
-//     res.json({ message: 'Form submitted successfully' });
-// } catch (error) {
-//     console.error( error);
-//     res.status(500).json({ message: 'Internal Server Error' });
-// }
-// })
-
-console.log(PORT);
 
 
 app.listen(PORT, ()=>{
