54d4a3f94c44d2fd49b8442f45d338846bed76c6
MFarooq22 <104709330+MFarooq22@users.noreply.github.com>
2023-11-11 00:35:33 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,21 +2,29 @@
 require('dotenv').config();
 const express = require('express');
 require('./DBConn/__db');
-
+const bodyParser = require('body-parser');
 const UserRoutes = require("./routes/User");
+const cors = require('cors');
 
 // Initialize Express app
 const app = express();
 
 // middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.json());
+app.use(cors());         // to avoid cors errors
+
 app.use('/api', UserRoutes);
+
 
 // Define the port for your server
 const PORT = process.env.PORT || 8000;
 
 
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server start at port no ${PORT}`);
 })
+
+
