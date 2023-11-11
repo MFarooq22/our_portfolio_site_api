90b87a539c360cdfa907d9ed4679594622374ec0
MFarooq22 <104709330+MFarooq22@users.noreply.github.com>
2023-11-11 16:38:19 +0500

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 // const User = require("../controllers/User");
-const {test} = require('../controllers/User');
 const {contactForm} = require('../controllers/contactForm');
 const {menu} = require('../controllers/menu');
 
 
-router.route('/test').get(test);
 router.route('/contact').post(contactForm);
 router.route('/menu').post(menu);
 
