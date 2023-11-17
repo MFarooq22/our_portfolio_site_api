9a2f286cd9c707fe7044190bde35878376ff09a7
MFarooq22 <104709330+MFarooq22@users.noreply.github.com>
2023-11-17 21:43:11 +0500

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const {contactForm} = require('../controllers/contactForm');
 const {menu} = require('../controllers/menu');
 const {createHome} = require("../controllers/home");
+const {resume} = require('../controllers/resume')
 const upload = require('../uploadFile');
 
 
 router.route('/contact').post(contactForm);
 router.route('/menu').post(menu);
 router.post('/home', upload.single('image'), createHome)
+router.route('/resume').post(resume);
 module.exports = router;
